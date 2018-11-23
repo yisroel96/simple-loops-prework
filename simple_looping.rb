7587a3f653d4b18f9881ea85542699f0940158d0
witty-collection-3981 <yisroel96@gmail.com>
2018-11-23 01:20:42 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -33,6 +33,7 @@ def while_iterator(number_of_times)
   # hint: user a counter to tell the while loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
 
+  count = 
   while count <= number_of_times
     puts phrase
     count += 1
